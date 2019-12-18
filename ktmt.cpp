f06fbdb019ba8c7f3355d4357475d8f523f787f2
hoaison306 <42867147+hoaison306@users.noreply.github.com>
2019-12-18 10:31:02 +0700

    Add computer architecture assignment code ktmt.cpp

---
+++ ktmt.cpp
@@ -0,0 +1,29 @@
+﻿#include<stdio.h>
+int isPrime(int n) {
+	if (n <= 1) return 0;
+	for (int i = 2; i < n; i++) {
+		if (n%i == 0) { return 0; }
+	}
+	return 1;
+}
+
+int FindPrimeMax(int arr[],int size) {
+	int max = 0;
+	for (int i = 0; i < size; i++) {
+		if (isPrime(arr[i])==1) {
+			if (arr[i] > max) {
+				max = arr[i];
+			}
+		}
+	}
+	if (max != 0) { return max; }
+	else return -1;
+}
+
+int main() {
+	
+	int arr[6] = {0,13,6,1,4,2};
+	printf("%d",FindPrimeMax(arr,6));
+	return 0;
+}
+
